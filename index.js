be084c080458f765c057961553a03be2adb7a7f3
m.vidmanov <vidmanov1mihail3@gmail.com>
2024-08-09 12:40:21 +0300

    Удалил лишние элементы

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
 let ball = document.querySelector('.ball');
-let upButton = document.querySelector('#up');
-let downButton = document.querySelector('#down');
-let leftButton = document.querySelector('#left');
+
 let rightButton = document.querySelector('#right');
+let restartButton = document.querySelector('#restart');
 
-upButton.addEventListener('click', function(e) {
-    let goUp = ball.offsetTop;
-    ball.style.top = goUp - 40 + "px";
-});
+let startingPositionBall = ball.offsetLeft
 
-downButton.addEventListener('click', function(e) {
-    let goDown = ball.offsetTop;
-    ball.style.top = goDown + 40 + "px";
-});
-
-leftButton.addEventListener('click', function(e) {
-    let goLeft = ball.offsetLeft;
-    ball.style.left = goLeft - 40 + "px";
-});
+let stepBall = 10;
 
 rightButton.addEventListener('click', function(e) {
     let goRight = ball.offsetLeft;
-    ball.style.left = goRight + 40 + "px";
+    ball.style.left = goRight + stepBall + "px";
+});
+
+restartButton.addEventListener('click', function(e) {
+    ball.style.left = startingPositionBall + "px";
 });
