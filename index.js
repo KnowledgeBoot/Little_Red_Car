d36c25b82ad9d5e24cbbe24ee0999306b6344f41
m.vidmanov <vidmanov1mihail3@gmail.com>
2024-08-13 17:02:14 +0300

    Добавил текст с текущим местоположением квадрата
    Добавил стили для отображения текста
    Добавил функции для изменения и сброса текста

--- index.js
+++ index.js
@@ -4,14 +4,29 @@ let rightButton = document.querySelector('#right');
 let restartButton = document.querySelector('#restart');
 
 let startingPositionBall = ball.offsetLeft
+let currentPositionBall = startingPositionBall;
 
 let stepBall = 10;
 
 rightButton.addEventListener('click', function(e) {
     let goRight = ball.offsetLeft;
     ball.style.left = goRight + stepBall + "px";
+    currentPositionBall += stepBall;
 });
 
 restartButton.addEventListener('click', function(e) {
     ball.style.left = startingPositionBall + "px";
 });
+
+function text_out() {
+    var p;
+    p = document.getElementById('text_change');
+    p.innerHTML = currentPositionBall + stepBall;
+}
+
+function text_restsrt() {
+    var restart;
+    restart = document.getElementById('text_change');
+    currentPositionBall = startingPositionBall
+    restart.innerHTML = currentPositionBall;
+}
