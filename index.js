a19591418a4eb22eeaf7f33bf499e5ea3e6f0457
m.vidmanov <vidmanov1mihail3@gmail.com>
2024-08-14 12:13:15 +0300

    Почистил код
    ---
    Убрал id из кнопок
    Убрал эвенты и перенёс функционал из них в функции

--- index.js
+++ index.js
@@ -1,38 +1,31 @@
 let ball = document.querySelector('.ball');
 
-let rightButton = document.querySelector('#right');
-let restartButton = document.querySelector('#restart');
-
 let startingPositionBall = ball.offsetLeft
 let currentPositionBall = startingPositionBall;
 
 let stepBall = 10;
 
-rightButton.addEventListener('click', function(e) {
+function text_out() {
     let goRight = ball.offsetLeft;
     
     if (currentPositionBall < 145) {
         ball.style.left = goRight + stepBall + "px";
         currentPositionBall += stepBall;
-    } else {
+    } 
+
+    if (currentPositionBall >= 145) {
         document.getElementById('img').style.display = "block";
     }
-});
 
-restartButton.addEventListener('click', function(e) {
-    ball.style.left = startingPositionBall + "px";
-    document.getElementById('img').style.display = "none";
-});
-
-function text_out() {
-    var p;
-    p = document.getElementById('text_change');
-    p.innerHTML = currentPositionBall + stepBall;
+    var p = document.getElementById('text_change');
+    p.innerHTML = currentPositionBall;
 }
 
 function text_restsrt() {
-    var restart;
-    restart = document.getElementById('text_change');
+    ball.style.left = startingPositionBall + "px";
+    document.getElementById('img').style.display = "none";
+
+    var restart = document.getElementById('text_change');
     currentPositionBall = startingPositionBall
     restart.innerHTML = currentPositionBall;
 }
